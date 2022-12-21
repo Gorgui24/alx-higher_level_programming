155f725b06cf0b13cbba92743806b77d61ea5ff8
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-12-21 13:44:18 -0800

    Alx Higher Level programming

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,12 +5,19 @@ import math
 class MagicClass:
     """Represent class"""
     def __init__(self, radius=0):
+        """Initialize MagicClass
+        Arg:
+            radius: (float or int): the radius...
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
         def area(self):
+            """Return the area of the MagicClass"""
             return (self.__radius ** 2 * math.pi)
+
         def circonf(self):
+            """Return the circonference of the magic class"""
             return (2 * math.pi * self.__radius)
