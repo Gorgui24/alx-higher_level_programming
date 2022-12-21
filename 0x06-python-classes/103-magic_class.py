ecec90d18993535196d72dee99a3fc500a5010b6
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-12-21 13:47:00 -0800

    Alx Higher Level programming

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,10 +14,9 @@ class MagicClass:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
-        def area(self):
-            """Return the area of the MagicClass"""
-            return (self.__radius ** 2 * math.pi)
-
-        def circonf(self):
-            """Return the circonference of the magic class"""
-            return (2 * math.pi * self.__radius)
+    def area(self):
+        """Return the area of the MagicClass"""
+        return (self.__radius ** 2 * math.pi)
+    def circonf(self):
+        """Return the circonference of the magic class"""
+        return (2 * math.pi * self.__radius)
