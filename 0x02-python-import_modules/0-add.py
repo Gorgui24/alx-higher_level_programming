efe9889a0c7b7aacf26e50232fd3f646d7cde21e
Gorgui24 <fayesouleymanegorgui@gmail.com>
2022-12-26 12:21:23 -0800

    Alx Higher Level Programming

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Print the the sum of 1 and 2"""
 from add_0 import add
-a, b = 1, 2
-sumAB = add(a, b)
-print("{} + {} = {}".format(a, b, sumAB))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b))
