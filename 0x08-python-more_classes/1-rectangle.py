822bb1312172bc111e1080166b40f4f07db98329
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-01-02 14:53:48 -0800

    Alx Higher Level Programming

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
     def width(self):
         """Get/Set la largeur du rectangle"""
         return self.__width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -24,14 +25,16 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+    
     @property
     def height(self):
         """Get/Set la longueur du rectangle"""
         return self.__height
+    
     @height.setter
     def height(self, value):
-        if not isinstance(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.__height = value
