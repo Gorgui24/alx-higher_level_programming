7c848b104b3319b6045e7be207d7fe852bcd34bc
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-01-15 07:12:07 -0800

    Alx Higher Level Programming

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""number_keys - define class to return lenght of list
+   args:
+        my_dict: number of list
+   Return: always number
+"""
+def number_keys(my_dict):
+    return len(my_dict.keys())
