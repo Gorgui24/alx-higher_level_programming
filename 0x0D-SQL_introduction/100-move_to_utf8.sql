e61f603d5f05276fa775d22a150a38ad4ce0cd4e
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-02-14 16:12:00 -0800

    Alx Higher Programming

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,11 +2,12 @@
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci
-
+-- convert Table first_table
 USE hbtn_0c_0;
+ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
+-- Convert Field name in first_tablei
 USE hbtn_0c_0;
 ALTER TABLE first_table
 CHANGE name name VARCHAR(256)
