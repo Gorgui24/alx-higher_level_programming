5c939cae47b378eca57bf590669aeb29dda00e22
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-02-14 16:16:42 -0800

    Alx Higher Programming

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- a script that converts hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci
+COLLATE = utf8mb4_unicode_ci;
 -- convert Table first_table
 USE hbtn_0c_0;
 ALTER TABLE first_table
