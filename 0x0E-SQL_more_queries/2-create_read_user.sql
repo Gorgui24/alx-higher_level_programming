c9d3702ac4ca92482fdc066ea2ef78fff67e8529
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-02-16 15:12:11 -0800

    Alx Higher Level Programming

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- a script that creates the database 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED TO 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
