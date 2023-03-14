a63254442e50779fcbba42aff9c12ae14b803371
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-03-13 17:25:41 -0700

    Alx Javascript

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+module.exports = {
+  addMeMaybe: function (x, theFunction) {
+    return theFunction(x + 1);
+  }
+};
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const argmt = process.argv.length - 2;
-if (argmt == 0)
+if (argmt === 0) {
 	console.log('No argument');
-else if (argmt == 1)
+} else if (argmt === 1) {
 	console.log('Argument found');
-else
+} else {
 	console.log('Arguments found');
+}
